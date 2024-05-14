82329a991e5335ef4ff5549619f3a31973badadd
JorderGomes <jordernoveau@gmail.com>
2024-05-14 13:44:49 -0300

    add action to form contact

--- script/index.js
+++ script/index.js
@@ -2,3 +2,26 @@ $("#menu-btn").on("click", function (event) {
     $("#open-menu").toggle();
     $("#close-menu").toggle();
 })
+
+$("#submit-contact").click(sendFeedback);
+
+function sendFeedback(event) {
+    event.preventDefault();
+    
+    const contact = {
+        "nome": $('#inputName').val(),
+        "email": $('#inputEmail').val(),
+        "mensagem": $('#inputMessage').val()
+    }
+    
+    $("form input[type=text], form input[type=email], form textarea").val("");
+    
+    $.ajax({
+        url: "https://formsubmit.co/ajax/fdf9e996f6ba19a8f62f7456094c4041",
+        method: "POST",
+        data: contact,
+        dataType: "json"
+    });
+
+    console.log(contact);
+}
